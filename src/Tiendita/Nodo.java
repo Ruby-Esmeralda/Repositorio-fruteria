11c04529be44f3634616eb95a5fd107d72ff42e3
rospa <ros.paolapech@gmail.com>
2025-04-09 14:59:38 -0600

    Termine la clase Nodo

---
+++ src/Tiendita/Nodo.java
@@ -0,0 +1,25 @@
+package Tiendita;
+
+public class Nodo {
+	private String Nombre;
+	private float Peso;
+	private String Color;
+	public Nodo Siguiente;
+	
+	public Nodo (String nombre, float peso, String color) {
+		Nombre=nombre;
+		Peso=peso;
+		Color=color;
+		Siguiente=null;		
+	}
+	
+	public String getNombre() {
+		return Nombre;
+	}
+	public float getPeso(){
+		return Peso;
+	}
+	public String getColor() {
+		return Color;
+	}
+}
