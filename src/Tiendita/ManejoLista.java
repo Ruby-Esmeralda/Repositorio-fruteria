0d359b947187280f1bef1add9c2d823d1cd578b8
Ruby-Esmeralda <ruby.estrella.775@gmail.com>
2025-04-09 19:15:07 -0600

    Agregar el correcto ManejoLista a mi Rama

--- src/Tiendita/ManejoLista.java
+++ src/Tiendita/ManejoLista.java
@@ -1,21 +1,40 @@
 package Tiendita;
 
 public class ManejoLista {
-		public Nodo primero;
-		public Nodo ultimo;
+	public Nodo primero;
+	public Nodo ultimo;
+	
+	public ManejoLista (String String) {
+		primero=null;
+		ultimo=null;
+	}
+	public void guardar() {
 		
-		public ManejoLista (String String) {
-				primero=null;
-				ultimo=null;
+	}
+	public void buscar() {
+		
+	}
+	public void eliminar(String nombreFruta) {
+		if (primero==null) return;
+		
+		if(primero.getNombre().equalsIgnoreCase(nombreFruta)) {
+			primero=primero.Siguiente;
+			if(primero==null) {
+			ultimo=null;
 		}
-		public void guardar() {
-			
+		return;
+	}
+	
+	Nodo aux= primero ;
+	while(aux.Siguiente !=null && !aux.Siguiente.getNombre().equalsIgnoreCase(nombreFruta)) {
+	aux=aux.Siguiente;
+	}
+	if(aux.Siguiente !=null) {
+		aux.Siguiente=aux.Siguiente.Siguiente;
+		if(aux.Siguiente==null) {
+			ultimo=aux;
+			}
 		}
-		public void buscar() {
-			
-		}
-		public void eliminar(String nombreFruta) {
-				
-		}
-
+	}
 }
+
