7456e7af38cb5198d60b9943a6ee472b3145b748
Ruby-Esmeralda <ruby.estrella.775@gmail.com>
2025-04-09 19:06:04 -0600

    Pasar el ManejoLista a mi Rama

---
+++ src/Tiendita/ManejoLista.java
@@ -0,0 +1,21 @@
+package Tiendita;
+
+public class ManejoLista {
+		public Nodo primero;
+		public Nodo ultimo;
+		
+		public ManejoLista (String String) {
+				primero=null;
+				ultimo=null;
+		}
+		public void guardar() {
+			
+		}
+		public void buscar() {
+			
+		}
+		public void eliminar(String nombreFruta) {
+				
+		}
+
+}
